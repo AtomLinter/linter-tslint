5c58856a0806a38019ee597f6d4fc14faec31d8a
Clay Miller <clay@smockle.com>
2017-04-19 21:14:47 -0700

    Add config option (enableSemanticRules) to control when program is passed to linter

--- spec/linter-tslint-spec.js
+++ spec/linter-tslint-spec.js
@@ -3,9 +3,7 @@
 import * as path from 'path';
 
 const validPath = path.join(__dirname, 'fixtures', 'valid', 'valid.ts');
-const validTypecheckedPath = path.join(__dirname, 'fixtures', 'valid-typechecked', 'valid-typechecked.ts');
 const invalidPath = path.join(__dirname, 'fixtures', 'invalid', 'invalid.ts');
-const invalidTypecheckedPath = path.join(__dirname, 'fixtures', 'invalid-typechecked', 'invalid-typechecked.ts');
 
 describe('The TSLint provider for Linter', () => {
   const lint = require('../lib/main.js').provideLinter().lint;
@@ -28,14 +26,6 @@ describe('The TSLint provider for Linter', () => {
     );
   });
 
-  it('finds nothing wrong with a valid file (typechecked)', () => {
-    waitsForPromise(() =>
-      atom.workspace.open(validTypecheckedPath).then(editor => lint(editor)).then((messages) => {
-        expect(messages.length).toBe(0);
-      }),
-    );
-  });
-
   it('handles messages from TSLint', () => {
     const expectedMsg = 'semicolon - Missing semicolon';
     waitsForPromise(() =>
@@ -50,7 +40,46 @@ describe('The TSLint provider for Linter', () => {
     );
   });
 
-  it('handles messages from TSLint (typechecked)', () => {
+  it('handles undefined filepath', () => {
+    waitsForPromise(() =>
+      atom.workspace.open().then(editor => lint(editor)).then((result) => {
+        expect(result).toBeNull();
+      }),
+    );
+  });
+});
+
+const validTypecheckedPath = path.join(__dirname, 'fixtures', 'valid-typechecked', 'valid-typechecked.ts');
+const invalidTypecheckedPath = path.join(__dirname, 'fixtures', 'invalid-typechecked', 'invalid-typechecked.ts');
+
+describe('The TSLint provider for Linter (with semantic rules)', () => {
+  const lint = require('../lib/main.js').provideLinter().lint;
+
+  beforeEach(() => {
+    atom.workspace.destroyActivePaneItem();
+
+    waitsForPromise(() =>
+      Promise.all([
+        atom.packages.activatePackage('linter-tslint'),
+      ]),
+    );
+
+    atom.config.set('linter-tslint.enableSemanticRules', true);
+  });
+
+  afterEach(() => {
+    atom.config.set('linter-tslint.enableSemanticRules', false);
+  });
+
+  it('finds nothing wrong with a valid file', () => {
+    waitsForPromise(() =>
+      atom.workspace.open(validTypecheckedPath).then(editor => lint(editor)).then((messages) => {
+        expect(messages.length).toBe(0);
+      }),
+    );
+  });
+
+  it('handles messages from TSLint', () => {
     const expectedMsg = 'no-unused-variable - \'foo\' is declared but never used.';
     waitsForPromise(() =>
       atom.workspace.open(invalidTypecheckedPath).then(editor => lint(editor)).then((messages) => {
@@ -60,14 +89,6 @@ describe('The TSLint provider for Linter', () => {
         expect(messages[0].text).toBe(expectedMsg);
         expect(messages[0].filePath).toBe(invalidTypecheckedPath);
         expect(messages[0].range).toEqual([[1, 8], [1, 11]]);
-      }),
-    );
-  });
-
-  it('handles undefined filepath', () => {
-    waitsForPromise(() =>
-      atom.workspace.open().then(editor => lint(editor)).then((result) => {
-        expect(result).toBeNull();
       }),
     );
   });
