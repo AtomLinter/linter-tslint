07b5725b18465dd24afe823d276069ed1dd44a28
Steven Hobson-Campbell <xapphire13@live.com>
2017-04-25 13:11:52 -0700

    Logging worker error to console. Making specs async.

--- spec/linter-tslint-spec.js
+++ spec/linter-tslint-spec.js
@@ -1,57 +1,48 @@
 'use babel';
 
 import * as path from 'path';
+import { beforeEach, it } from 'jasmine-fix'; // eslint-disable-line import/no-extraneous-dependencies
+import linterTslint from '../lib/main';
 
 const invalidPath = path.join(__dirname, 'fixtures', 'invalid', 'invalid.ts');
 const noConfigPath = path.join(__dirname, 'fixtures', 'no-config', 'noConfig.ts');
 const validPath = path.join(__dirname, 'fixtures', 'valid', 'valid.ts');
 
 describe('The TSLint provider for Linter', () => {
-  const lint = require('../lib/main.js').provideLinter().lint;
+  const lint = linterTslint.provideLinter().lint;
 
-  beforeEach(() => {
-    atom.workspace.destroyActivePaneItem();
-
-    waitsForPromise(() =>
-      Promise.all([
-        atom.packages.activatePackage('linter-tslint'),
-      ]),
-    );
+  beforeEach(async () => {
+    await atom.packages.activatePackage('linter-tslint');
   });
 
-  it('finds nothing wrong with a valid file', () => {
-    waitsForPromise(() =>
-      atom.workspace.open(validPath).then(editor => lint(editor)).then((messages) => {
-        expect(messages.length).toBe(0);
-      }),
-    );
-  });
+  describe('When the package is activated', () => {
+    it('finds nothing wrong with a valid file', async () => {
+      const editor = await atom.workspace.open(validPath);
+      const messages = await lint(editor);
+      expect(messages.length).toBe(0);
+    });
 
-  it('handles messages from TSLint', () => {
-    const expectedMsg = 'semicolon - Missing semicolon';
-    waitsForPromise(() =>
-      atom.workspace.open(invalidPath).then(editor => lint(editor)).then((messages) => {
-        expect(messages.length).toBe(1);
-        expect(messages[0].type).toBe('warning');
-        expect(messages[0].html).not.toBeDefined();
-        expect(messages[0].text).toBe(expectedMsg);
-        expect(messages[0].filePath).toBe(invalidPath);
-        expect(messages[0].range).toEqual([[0, 14], [0, 14]]);
-      }),
-    );
-  });
+    it('handles messages from TSLint', async () => {
+      const expectedMsg = 'semicolon - Missing semicolon';
+      const editor = await atom.workspace.open(invalidPath);
+      const messages = await lint(editor);
+      expect(messages.length).toBe(1);
+      expect(messages[0].type).toBe('warning');
+      expect(messages[0].html).not.toBeDefined();
+      expect(messages[0].text).toBe(expectedMsg);
+      expect(messages[0].filePath).toBe(invalidPath);
+      expect(messages[0].range).toEqual([[0, 14], [0, 14]]);
+    });
 
-  it('handles undefined filepath', () => {
-    waitsForPromise(() =>
-      atom.workspace.open().then(editor => lint(editor)).then((result) => {
-        expect(result).toBeNull();
-      }),
-    );
-  });
+    it('handles undefined filepath', async () => {
+      const editor = await atom.workspace.open();
+      const result = await lint(editor);
+      expect(result).toBeNull();
+    });
 
-  it('validates even when there is no tslint.json', () => {
-    waitsForPromise(() =>
-      atom.workspace.open(noConfigPath).then(editor => lint(editor)),
-    );
+    it('finishes validatation even when there is no tslint.json', async () => {
+      const editor = await atom.workspace.open(noConfigPath);
+      await lint(editor);
+    });
   });
 });
