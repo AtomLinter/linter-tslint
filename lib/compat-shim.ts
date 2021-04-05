8a9404bf37686990394d91842496eab9610a0d3c
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-05 06:10:14 -0500

    chore: better type for compat-shim

--- lib/compat-shim.ts
+++ lib/compat-shim.ts
@@ -1,12 +1,14 @@
 import type * as Tslint from "tslint";
 
+type LinterTslintV3 = (filePath: string, text: string, configuration: Tslint.ILinterOptions) => { lint: () => Tslint.LintResult[] }
+
 /**
  * Shim for TSLint v3 interoperability
  * @param {Function} Linter TSLint v3 linter
  * @return {Function} TSLint v4-compatible linter
  */
-export function shim(Linter: Function): typeof Tslint.Linter {
-  function LinterShim(options) {
+export function shim(Linter: LinterTslintV3): typeof Tslint.Linter {
+  function LinterShim(options: Tslint.ILinterOptions) {
     this.options = options;
     this.results = {};
   }
@@ -17,9 +19,9 @@ export function shim(Linter: Function): typeof Tslint.Linter {
   // Assign instance methods
   LinterShim.prototype = {
     ...Linter.prototype,
-    lint(filePath, text, configuration) {
-      const options = { ...this.options, configuration };
-      const linter = new Linter(filePath, text, options);
+    lint(filePath: string, text: string, configuration: Tslint.ILinterOptions) {
+      const options : Tslint.ILinterOptions = { ...this.options, configuration };
+      const linter = new Linter(filePath, text, options) as ReturnType<LinterTslintV3>;
       this.results = linter.lint();
     },
     getResult() {
